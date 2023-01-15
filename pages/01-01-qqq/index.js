a68b67400e3a53c2d39185f4dcf0e2b9069f5194
jakesungjaekim <codejakecarry@gmail.com>
2023-01-16 01:47:47 +0900

    ADD: Day01 끝; emotion사용해보기

---
+++ pages/01-01-qqq/index.js
@@ -0,0 +1,12 @@
+export default function Qqq () {
+
+    return(
+        <div>
+            <div>
+                안녕하세요? 이 페이지는 qqq페이지입니다만?
+            </div>
+            <button>클릭클릭!!</button>
+        </div>
+    )
+}
+
---
+++ pages/01-02-emotion/index.js
@@ -0,0 +1,16 @@
+import { Email,EmailInput } from '../../styles/emotion'
+
+
+export default function EmotionPage() {
+
+    // 이곳은 JS를 쓰는 곳 입니다.
+
+    return(
+        <div>
+            <Email>이메일</Email>
+            <EmailInput type="text" />
+            <button>클릭클릭!!</button>
+        </div>
+    )
+}
+
